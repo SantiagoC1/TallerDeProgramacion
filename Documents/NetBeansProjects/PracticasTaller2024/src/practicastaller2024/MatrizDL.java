1c1f6939da6d50ba3e0f41044c7fbe80fe6000c3
santi <1caceres.santiago5@gmail.com>
2024-09-24 20:56:29 -0300

    modify matrizDL

--- Documents/NetBeansProjects/PracticasTaller2024/src/practicastaller2024/MatrizDL.java
+++ Documents/NetBeansProjects/PracticasTaller2024/src/practicastaller2024/MatrizDL.java
@@ -13,50 +13,39 @@ import PaqueteLectura.*;
 public class MatrizDL {
     public static void main(String[] args) {
         int posFi=5,posCol=9;
-        int i=0,j=0;
+        int i=-1,j,cant=0,x=0,y;
         //dimension logica
             int [][]bingo1=new int[posFi][posCol];
             System.out.println("Ingrese un dato");
             int dato=Lector.leerInt();
-            i=-1;
-                while(i<5&&dato!=0){
-                    i++;
-                    j=-1;
-                    while(j<9&&dato!=0){
-                        
-                        if (dato!=0){
-                            j++;
-                            System.out.println(i+","+j);
-                            bingo1[i][j]=dato;
-                        }
-                        
-                        System.out.println("Ingrese un dato");
-                        dato=Lector.leerInt();
-                    }
+            while (i<5&&dato!=0){
+               i++;
+               j=0;
+               while(j<9&&dato!=0){
+                   System.out.println(i+","+j);
+                    bingo1[i][j]=dato;
                     
+                    cant++;
+                    j++;
                     System.out.println("Ingrese un dato");
                     dato=Lector.leerInt();
-                }
-                
-                int x=0;
-                while (x<=i){
-                    int y=0;
-                    while(y<=j){
-                         System.out.print("("+x+","+y+") "+bingo1[x][y]+" | ");
-                         y++;
+               }
+            }
+            System.out.println("CANTIDAD"+cant);
+            //Imprimir matriz
+            while(cant>0){
+                while(cant>0&&x<5){
+                    y=0;
+                    while(cant>0&&y<9){
+                        System.out.println(bingo1[x][y]);
+                        y++;
+                        cant--;
                     }
                     x++;
                 }
-                
-                /*for (i=0;i<5;i++){
-                System.out.println("-----------------------------------------------");
-                for (j=0;  j< 9; j++) {
-                System.out.print("("+i+","+j+") "+bingo1[i][j]+" | ");
-                }
-                System.out.println(" ");
-                }
-                System.out.println("---------------------------------------------------------------------------");*/
-            
-    }
+            }
+
+    
+}
     
 }
